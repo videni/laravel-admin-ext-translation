aaaca2c41d2b22ed0d1fb9a9ad0052ec3dfe0dbc
Bo.Yi <bo.yi@kankanapp.com.cn>
2018-02-27 17:26:35 +0800

    [fix]safely ignore config setup

--- src/LocaleModel.php
+++ src/LocaleModel.php
@@ -25,17 +25,21 @@ class LocaleModel extends Model
     public static function boot()
     {
         parent::boot();
-        $locales = [];
-        $locales_trans = [];
-        $locales = array_flatten(static::select('code')->get()->toarray());
-        $locales_trans = static::select(['code', 'name'])->get()->mapWithKeys(function($row){
-            return [$row->code => $row->name];
-        })->toarray();
-        // dd($locales, $locales_trans);
-        config([
-            'admin.extensions.translation.locales' => $locales,
-            'admin.extensions.translation.locales_trans' => $locales_trans,
-        ]);
-        // dd(Translation::config('locales'), Translation::config('locales_trans'));
+        try {
+            $locales = [];
+            $locales_trans = [];
+            $locales = array_flatten(static::select('code')->get()->toarray());
+            $locales_trans = static::select(['code', 'name'])->get()->mapWithKeys(function($row){
+                return [$row->code => $row->name];
+            })->toarray();
+            // dd($locales, $locales_trans);
+            config([
+                'admin.extensions.translation.locales' => $locales,
+                'admin.extensions.translation.locales_trans' => $locales_trans,
+            ]);
+            // dd(Translation::config('locales'), Translation::config('locales_trans'));
+        } catch (\Exception $e) {
+            // safely ignore this loading error when no db connection available
+        }
     }
 }
